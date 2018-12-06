40a6f1e98c7bd35b6687bb81887d66986e843c16
DenisKruglik <d.kruglik11011@gmail.com>
2018-12-07 00:54:11 +0300

    Implemented check duplicit algorithm

--- Lab7/algorithms.py
+++ Lab7/algorithms.py
@@ -1,4 +1,4 @@
-def dfs(graph, start):
+def dfs(graph, start=0):
     '''Depth-first search
 
         Args:
@@ -8,8 +8,8 @@ def dfs(graph, start):
         Returns:
             tuple: tuple contains a list of straight arcs, a list of converse arcs and a dictionary of labels'''
     d = {v: None for v in range(graph.vertex_count) if v != start}
-    k = 1
-    d[start] = k
+    k = 0
+    d[k] = k
     q = [start]
     straight = []
     converse = []
@@ -31,3 +31,39 @@ def dfs(graph, start):
             q.pop()
 
     return straight, converse, d
+
+
+def check_duplicity(graph, start=0):
+    current_label = 1
+    stack = [(start, current_label)]
+    visited = [start]
+    current_label += 1
+    min_converse = None
+    straight = []
+    converse = []
+
+    while len(stack) > 0:
+        v, label = stack[-1]
+
+        for u, e in enumerate(graph.matrix[v]):
+            if e is not None and u not in visited:
+                stack.append((u, current_label))
+                visited.append(u)
+                current_label += 1
+                straight.append({v, u})
+                if len(list(filter(lambda edge: start in edge, straight))) > 1:
+                    return False
+                break
+        else:
+            for u, e in enumerate(graph.matrix[v]):
+                if e is not None and u in visited and {v, u} not in straight and {v, u} not in converse:
+                    converse.append({v, u})
+                    l = next(filter(lambda i: i[0] == u, stack))[1]
+                    min_converse = l if min_converse is None else min(min_converse, l)
+
+            if label > 1 and (min_converse is None or min_converse >= label):
+                return False
+
+            stack.pop()
+
+    return True
