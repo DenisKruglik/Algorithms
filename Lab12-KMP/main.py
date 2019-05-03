6fdc5b93e9ceca17fc3ad522ed7243e51709d7ec
ArtimenyaEgor <artimenyaegor@gmail.com>
2019-05-03 10:48:13 +0300

    Added dynamic substring search

--- Lab12-KMP/main.py
+++ Lab12-KMP/main.py
@@ -1,4 +1,5 @@
-import readchar, sys
+import tkinter as tk, re
+from tkinter import simpledialog
 
 
 def build_dfa(pattern):
@@ -26,3 +27,34 @@ def substring(string, dfa):
         if state == end:
             return i - state + 1
     return -1
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Substring search')
+    root.geometry("1000x300+300+250")
+    state = 0
+    input_str = ''
+    label = tk.Label(text=input_str, justify=tk.LEFT)
+
+    def onkeypress(event):
+        if re.match('^[a-zA-Z0-9]*$', event.char):
+            global state, label, input_str
+            state = dfa[state].get(event.char, 0)
+            if state == pat_size:
+                state = 0
+                label.config(text=input_str[:-pat_size+1], textvariable=None)
+                input_str = ''
+                tk.Label(text=pattern, justify=tk.LEFT, fg='red').pack(side=tk.LEFT)
+                label = tk.Label(textvariable=input_str, justify=tk.LEFT)
+                label.pack(side=tk.LEFT)
+            else:
+                input_str += event.char
+                label.config(text=input_str)
+
+    root.bind('<KeyPress>', onkeypress)
+    label.pack(side=tk.LEFT)
+    pattern = simpledialog.askstring('Pattern', 'Enter search pattern')
+    pat_size = len(pattern)
+    dfa = build_dfa(pattern)
+    root.mainloop()
